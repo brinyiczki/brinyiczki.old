9c8dc1200701ed1af10369677aeeca15ea816f45
brinyiczki <brinyiczki@proton.me>
2023-11-29 18:38:31 +0100

    sfsdvxv

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,7 +8,7 @@ import {themes as prismThemes} from 'prism-react-renderer';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'ads',
+  title: 'sadagsgsagfsafsaf',
   tagline: 'sdasadasfsdg are cool',
   favicon: 'img/favicon.ico',
 
