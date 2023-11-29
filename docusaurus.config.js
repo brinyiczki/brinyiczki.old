8ee836db940ceedbb51ce4b9908faf831d9de7d8
brinyiczki <brinyiczki@proton.me>
2023-11-29 20:32:20 +0100

    config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,8 +8,8 @@ import {themes as prismThemes} from 'prism-react-renderer';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'wfsdfsdgsdg',
-  tagline: 'sdasadasfsdg are cool',
+  title: 'GRP',
+  tagline: 'LUA guru',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
