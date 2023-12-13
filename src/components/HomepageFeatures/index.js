3bad57cd41ebbbe686839d3943d9aedef18a02e1
brinyiczki <brinyiczki@proton.me>
2023-12-13 16:56:59 +0100

    config

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -62,3 +62,22 @@ export default function HomepageFeatures() {
     </section>
   );
 }
+
+
+
+
+Array.from(document.querySelectorAll(".menu__link")).forEach(a => {
+  a.addEventListener("click", function (e) {
+      const ripple = document.createElement("div"),
+          rect = a.getBoundingClientRect();
+      ripple.className = "animate",
+      ripple.style.left = `${e.x - rect.left}px`,
+      ripple.style.top = `${e.y - rect.top}px`,
+      ripple.style.background = `#${a.dataset.color !== undefined ? a.dataset.color : "bdc3c7"}`,
+      ripple.style.setProperty("--material-scale", a.offsetWidth),
+      a.append(ripple),
+      setTimeout(function () {
+          ripple.parentNode.removeChild(ripple)
+      }, 500)
+  })
+})
